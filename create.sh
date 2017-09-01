9f41d9aea5f481fa9417d14d5f95e8f707867277
Mike Meaders <mikemeaders@yahoo.com>
2017-09-01 16:13:28 -0400

    v1web

--- create.sh
+++ create.sh
@@ -1,1 +1,5 @@
-docker run -td -p 80:80 nginx
+docker run --name web -td -p 80:80 -v /home/pi/webfolder:/webfolder nginx
+docker cp default web:/etc/nginx/sites-available/
+docker cp index.nginx-debian.html web:/var/www/html/
+docker cp jc.jpg web:var/www/html/
+docker exec -d web service nginx start
